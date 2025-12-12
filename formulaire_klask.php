2e492b3b9c03b1d52264e6e7348450558d373a03
Khinyas <lucasar29000@gmail.com>
2025-12-12 15:45:21 +0100

    [FIX] deleted unnecessary script

--- formulaire_klask.php
+++ formulaire_klask.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="public/css/pages/formulaire_klask.css">
-    <script src="formulaire"></script>
+
 </head>
 <body data-theme="standard"> <div class="sphere sphere-1"></div>
     <div class="sphere sphere-2"></div>
