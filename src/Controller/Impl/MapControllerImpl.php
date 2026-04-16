bf584fbf8ecfc5dc75830449e3d619c202f1b676
Clement Gizardin <clemgiz@hotmail.fr>
2026-04-16 11:21:40 +0200

    [WIP] Implementing map

--- src/Controller/Impl/MapControllerImpl.php
+++ src/Controller/Impl/MapControllerImpl.php
@@ -1,9 +1,23 @@
 <?php
 
 namespace App\Controller\Impl;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 
 use App\Controller\MapController;
 
-class MapControllerImpl implements MapController
+class MapControllerImpl extends AbstractController implements MapController
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    #[Route('/map', name: 'app_map')]
+    public function index(): Response
+    {
+        return $this->render('map/index.html.twig');
+    }
 }
--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 interface MapController
 {
-
+    public function index(): Response;
 }
