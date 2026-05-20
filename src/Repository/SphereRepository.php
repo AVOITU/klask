7bcf4f018aed529118efe5fa646aabd830798592
Jennifer V. <147718561+JnniferV@users.noreply.github.com>
2026-05-20 22:51:10 +0200

    Refactor SphereRepository to manage Sphere entity
    Updated the SphereRepository to extend ServiceEntityRepository for the Sphere entity and added a method to find all spheres with activities.

--- src/Repository/SphereRepository.php
+++ src/Repository/SphereRepository.php
@@ -2,14 +2,27 @@
 
 namespace App\Repository;
 
-use App\Entity\Event;
+use App\Entity\Sphere;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<Sphere>
+ */
 class SphereRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Event::class);
+        parent::__construct($registry, Sphere::class);
+    }
+
+    /** @return Sphere[] */
+    public function findAllWithActivities(): array
+    {
+        return $this->createQueryBuilder('s')
+            ->leftJoin('s.activities', 'a')
+            ->addSelect('a')
+            ->getQuery()
+            ->getResult();
     }
 }
