4e27c7b45c41fe3aa4e0dd288ab6350a6c901f2e
Jennifer V. <147718561+JnniferV@users.noreply.github.com>
2026-05-27 13:05:50 +0200

    Remove unused method from GroupService interface

--- src/Service/GroupService.php
+++ src/Service/GroupService.php
@@ -2,12 +2,7 @@
 
 namespace App\Service;
 
-use App\Entity\Group;
-
 interface GroupService
 {
-    //public function findDistinctEstablishments(): array;
-    public function findById(int $groupId): ?Group;
-
     public function findGroupTotalScore(int $groupId): int;
 }
