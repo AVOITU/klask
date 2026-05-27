c728b832557ade9ba0a717ecefe272915141c26e
Jennifer V. <147718561+JnniferV@users.noreply.github.com>
2026-05-27 13:07:40 +0200

    Add savePosition method to MapService interface

--- src/Service/MapService.php
+++ src/Service/MapService.php
@@ -2,8 +2,13 @@
 
 namespace App\Service;
 
+use App\Entity\Activity;
+use App\Entity\Sphere;
+
 interface MapService
 {
-    /** @return array<int, array{id: int, name: string, color: string, centerX: float, centerY: float, size: float, activities: array}> */
+    // @return array<int, array{id: int, name: string, color: string, centerX: float, centerY: float, size: float, activities: array}>
     public function getPreparedSpheres(): array;
+
+    public function savePosition(Sphere|Activity $entity, float $x, float $y, ?float $radius = null): void;
 }
