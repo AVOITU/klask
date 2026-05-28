2dd0a6d7396ef2dd4f38299c3d34eb3c45f32195
Jennifer V. <147718561+JnniferV@users.noreply.github.com>
2026-05-28 11:56:51 +0200

    Remove configureAssets method

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -34,11 +33,6 @@ class DashboardController extends AbstractDashboardController
                 'accompanying'   => $g->setController(AccompanyingCrudController::class)->setAction(Action::INDEX)->generateUrl(),
             ],
         ]);
-    }
-
-    public function configureAssets(): Assets
-    {
-        return Assets::new()->addCssFile('styles/admin.css');
     }
 
     public function configureDashboard(): Dashboard
